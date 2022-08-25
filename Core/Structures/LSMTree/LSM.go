5fd802a941939ed02ba2288e6484d92bf4847417
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-08-25 16:23:54 +0200

    Added consturctor and method for uploading lsm tree. Also added helper function for finding index and level from file's name

---
+++ Core/Structures/LSMTree/LSM.go
@@ -0,0 +1,68 @@
+package LSMTree
+
+import (
+	"KeyValueEngine/Core/Structures/Memtable"
+	"KeyValueEngine/Core/Structures/SSTable"
+	"io/ioutil"
+	"strconv"
+)
+
+type LSM struct {
+	memtable Memtable.Memtable
+	ssTables [][]SSTable.SSTable
+	maxLevel uint16
+	maxTablesInLevel uint16
+}
+
+func InitLSM (memtable Memtable.Memtable, maxLevel, maxTablesInLevel uint16) (*LSM){
+	return &LSM{
+		memtable: memtable,
+		ssTables: make([][]SSTable.SSTable, maxLevel),
+		maxLevel: maxLevel,
+		maxTablesInLevel: maxTablesInLevel,
+	}
+}
+
+func (lsm *LSM) UploadData() {
+	files, _ := ioutil.ReadDir("data/data")
+
+	if len(files) == 0 {
+		return
+	}
+
+	tablesNumInLevel := make([]int, lsm.maxLevel)
+
+	for _, file := range files {
+		currentName := file.Name()
+
+		level, index := getLevelAndIndex(currentName)
+
+		if tablesNumInLevel[level] < index {
+			tablesNumInLevel[level] = index
+		}
+	}
+	numOfLevels := int(lsm.maxLevel)
+	for i := 0; i < numOfLevels; i++ {
+		if tablesNumInLevel[i] != 0 {
+			lsm.ssTables[i] = make([]SSTable.SSTable, tablesNumInLevel[i])
+		}else{
+			lsm.ssTables[i] = make([]SSTable.SSTable, 0)
+		}
+	}
+
+	for _, file := range files{
+		level, index := getLevelAndIndex(file.Name())
+		levelStr := strconv.Itoa(level)
+		indexStr := strconv.Itoa(index)
+		ssTable := SSTable.SSTable{
+			DataFilePath: "data/data/data_" + levelStr + "_" + indexStr + ".bin",
+			IndexFilePath: "data/index/index_" + levelStr + "_" + indexStr + ".bin",
+			SummeryFilePath: "data/summery/summery_" + levelStr + "_" + indexStr + ".bin",
+			FilterFilePath: "data/filer/filter_" + levelStr + "_" + indexStr + ".bin",
+			MetadataFilePath: "data/metadata/metadata_" + levelStr + "_" + indexStr + ".bin",
+			TOCFilePath: "data/TOC/toc_" + levelStr + "_" + indexStr + ".bin",
+		}
+		lsm.ssTables[level][index] = ssTable
+	}
+}
+
---
+++ Core/Structures/LSMTree/helperLSM.go
@@ -0,0 +1,19 @@
+package LSMTree
+
+import (
+	"strconv"
+	"strings"
+)
+
+func getLevelAndIndex (fileName string) (int, int) {
+	tokens := strings.Split(fileName, "_")
+	level, err := strconv.Atoi(tokens[1])
+	if err != nil {
+		panic(err)
+	}
+	index, err := strconv.Atoi(tokens[2])
+	if err != nil {
+		panic(err)
+	}
+	return level, index
+}
